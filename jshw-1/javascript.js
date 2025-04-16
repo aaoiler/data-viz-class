ced65e80bc327a8da81467e39bf6673608d017a5
aaoiler <aoiler@terpmail.umd.edu>
2025-04-16 15:58:30 -0400

    push

--- jshw-1/javascript.js
+++ jshw-1/javascript.js
@@ -22,10 +22,10 @@ var myChart = new Chart(ctx, {
           'rgb(255, 205, 86)',
           'rgb(201, 203, 207)',
           'rgb(56, 162, 232)',
-          '#8338ec',
-          rgb(85, 231, 139),
-          rgb(186, 142, 198),
-          rgb(193, 234, 33)
+          'rgb(191, 3, 3)',
+          'rgb(85, 231, 139)',
+          'rgb(186, 142, 198)',
+          'rgb(193, 234, 33)'
         ]
       }]
     },
@@ -38,7 +38,12 @@ var myChart = new Chart(ctx, {
         title: {
           display: true,
           text: 'Genres of the Top IMDB 50 Moives '
-        }
+
+        },
+            subtitle: {
+                display: true,
+                text: 'In the top 50 movies according to IMDB ratings, action movies were most common with 13 movies. This included movies like Inception and Lord of the Rings. The only horror movie that made the cut was Psycho. '
+            }
       }
     }
   });
